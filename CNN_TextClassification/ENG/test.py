6a0b416d7bc5529a67685099d40af9c2f979c2c3
chenng <wangchengo@126.com>
2018-08-14 09:59:07 +0800

    删除test.py

--- CNN_TextClassification/ENG/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import tensorflow as tf
-
-W = np.random.random((5, 9))
-input_x = [[1, 2, 3, 0], [2, 2, 3, 0]]
-embedded_chars = tf.nn.embedding_lookup(W, input_x)
-print(embedded_chars)
-embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
-print(embedded_chars_expanded)
