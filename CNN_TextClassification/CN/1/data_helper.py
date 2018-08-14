124c3996a7a5293f3104f808f467af9ac100a9d8
chenng <wangchengo@126.com>
2018-08-14 14:40:40 +0800

    添加中文垃圾邮件分类

---
+++ CNN_TextClassification/CN/1/data_helper.py
@@ -0,0 +1,56 @@
+import re
+import jieba
+import numpy as np
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string.strip('\n')
+    # string = re.sub("[A-Za-z0-9(),\!\?\'\`\,\'\。\，\－\:\４\Ｍ \\n\@ \：\？\....."
+    #                 "\（ \）\Ｑ\；\…\！\-\、\_____\】\【\^\&\+\-\=\*\~\/\２\６\１"
+    #                 "\Ｐ\Ｓ \“\”]", "", string)
+    string = re.sub(r"[^\u4e00-\u9fff]", " ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip()
+
+def cut_line(line):
+    line = clean_str(line)
+    seg_list = jieba.cut(line)
+    cut_words = " ".join(seg_list)
+    return cut_words
+
+
+
+def load_data_and_labels(positive_data_file, negative_data_file):
+    positive = []
+    negative = []
+    for line in open(positive_data_file,encoding='utf-8'):
+        positive.append(cut_line(line))
+    for line in open(negative_data_file,encoding='utf-8'):
+        negative.append(cut_line(line))
+    x_text = positive + negative
+
+    positive_label = [[0, 1] for _ in positive]  # 构造one-hot 标签[[0, 1], [0, 1], [0, 1], [0, 1],....]
+    negative_label = [[1, 0] for _ in negative]
+
+    y = np.concatenate([positive_label, negative_label], axis=0)
+
+    return x_text,y
+
+
+def gen_batch(x_train, y_train, begin, batch_size):
+    data_size = len(y_train)
+    start = (begin * batch_size) % data_size
+    end = min(start + batch_size, data_size)
+    x = x_train[start:end]
+    y = y_train[start:end]
+    return x, y
+
+
+
+
+
+# positive_data_file = './data/ham_100.utf8'
+# negative_data_file = './data/spam_100.utf8'
+
