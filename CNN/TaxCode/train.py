5f958085ff06cfe726e3c879930044f7e30b28ef
chenng <wangchengo@126.com>
2018-08-21 13:52:22 +0800

    更改内存限制（已经能正常运行）

--- CNN/TaxCode/train.py
+++ CNN/TaxCode/train.py
@@ -4,11 +4,10 @@ import os
 import time
 import datetime
 import data_helper
-from tensorflow.contrib import learn
 from text_cnn import TextCNN
 from config import FLAGS
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # 指定一个GPU
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # 指定一个GPU
 print('\n----------------Parameters--------------')  # 在网络训练之前，先打印出来看看
 for attr, value in (FLAGS.__flags.items()):
     print('{}={}'.format(attr.upper(), value))
@@ -16,11 +15,13 @@ for attr, value in (FLAGS.__flags.items()):
 # Load data and cut
 x_train_data, y = data_helper.load_data_and_labels(FLAGS.train_data_file, FLAGS.train_label_file)
 
-
 # Padding sentence
 padded_sentences_train, max_padding_length = data_helper.padding_sentence(
-    sentences=x_train_data, padding_sentence_length=FLAGS.padding_sentence_length)
-
+    sentences=x_train_data,
+    padding_sentence_length=FLAGS.padding_sentence_length,
+    padding_move=FLAGS.padding_move
+)
+print(padded_sentences_train[:10])
 x, vocabulary_len = data_helper.embedding_sentences(
     embedding_file=FLAGS.embedding_file, padded_sentences=padded_sentences_train,
     embedding_dimension=FLAGS.embedding_dimension)
@@ -45,7 +46,7 @@ print('train/dev split:{:d}/{:d}'.format(len(y_train), len(y_dev)))
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(allow_soft_placement=FLAGS.allow_soft_placement,
                                   log_device_placement=FLAGS.log_device_placement)
-    session_conf.gpu_options.per_process_gpu_memory_fraction = 0.6
+    session_conf.gpu_options.per_process_gpu_memory_fraction = 0.9
     session_conf.gpu_options.allow_growth = True
     sess = tf.Session(config=session_conf)
     with sess.as_default():
@@ -65,21 +66,19 @@ with tf.Graph().as_default():
     sess.run(tf.global_variables_initializer())
     last = datetime.datetime.now()
     for i in range(FLAGS.training_ite):
-        x, y = data_helper.gen_batch(x_train, y_train, i, FLAGS.batch_size)
-        feed_dic = {cnn.input_x: x, cnn.input_y: y, cnn.dropout_keep_prob: FLAGS.dropout}
+        x_batch, y_batch = data_helper.gen_batch(x_train, y_train, i, FLAGS.batch_size)
+
+        feed_dic = {cnn.input_x: x_batch, cnn.input_y: y_batch, cnn.dropout_keep_prob: FLAGS.dropout}
         _, loss, acc = sess.run([train_step, cnn.loss, cnn.accuracy], feed_dict=feed_dic)
-        print('-----------------',global_step)
         if (i % 100) == 0:
             now = datetime.datetime.now()
             print('loss:{},acc:{}---time:{}'.format(loss, acc, now - last))
             last = now
         if (i % 1000) == 0:
             feed_dic = {cnn.input_x: x_dev, cnn.input_y: y_dev, cnn.dropout_keep_prob: 1.0}
-            _, loss, acc = sess.run([train_step, cnn.loss, cnn.accuracy], feed_dict=feed_dic)
+            loss, acc = sess.run([cnn.loss, cnn.accuracy], feed_dict=feed_dic)
             print('-------------loss:{},acc:{}---time:{}--step:{}'.format(loss, acc, now - last, i))
         if (i % FLAGS.save_freq) == 0:
             saver.save(sess, os.path.join(FLAGS.model_save_path,
                                           FLAGS.model_name),
-                       global_step=global_step,)
-
-
+                       global_step=global_step, )
