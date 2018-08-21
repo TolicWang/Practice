6a57a187f0fe7e55b116a5ba51d997f15a6b1d16
chenng <wangchengo@126.com>
2018-08-21 13:53:41 +0800

    增加载入模型控制参数到config

--- CNN/TaxCode/eval.py
+++ CNN/TaxCode/eval.py
@@ -6,7 +6,9 @@ import data_helper
 x_test_data, y_test = data_helper.load_data_and_labels(FLAGS.test_data_file, FLAGS.test_label_file)
 
 padded_sentences_test, max_padding_length = data_helper.padding_sentence(
-    sentences=x_test_data, padding_sentence_length=FLAGS.padding_sentence_length)
+    sentences=x_test_data,
+    padding_sentence_length=FLAGS.padding_sentence_length,
+    padding_move=FLAGS.padding_move)
 
 x_test, vocabulary_len = data_helper.embedding_sentences(
     embedding_file=FLAGS.embedding_file, padded_sentences=padded_sentences_test,
@@ -25,7 +27,8 @@ cnn = TextCNN(sequence_length=FLAGS.padding_sentence_length,
 
 with tf.Session() as sess:
     saver = tf.train.Saver()
-    saver.restore(sess, './model/model.ckpt-502')
+    path = FLAGS.model_save_path + FLAGS.restore_model_name
+    saver.restore(sess, save_path=path)
     feed_dic = {cnn.input_x: x_test, cnn.input_y: y_test, cnn.dropout_keep_prob: FLAGS.dropout}
     acc = sess.run(cnn.accuracy, feed_dict=feed_dic)
     print('----acc:{}---'.format(acc))
