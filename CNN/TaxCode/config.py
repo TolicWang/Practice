187be7efcf7843f44de4e66a6bd94baec75accec
chenng <wangchengo@126.com>
2018-08-21 10:03:41 +0800

    增加模型保存路径

--- CNN/TaxCode/config.py
+++ CNN/TaxCode/config.py
@@ -9,7 +9,7 @@ tf.flags.DEFINE_string(flag_name='test_data_file', default_value='./data/test_te
                        docstring='test data')
 tf.flags.DEFINE_string(flag_name='test_label_file', default_value='./data/test_label.txt',
                        docstring='test label')
-tf.flags.DEFINE_string(flag_name='embedding_file', default_value='./data/sgns.merge.word',
+tf.flags.DEFINE_string(flag_name='embedding_file', default_value='./data/sgns.renmin.word',
                        docstring='test label')
 
 #   data processing parameters
@@ -23,6 +23,8 @@ tf.flags.DEFINE_float(flag_name='L2_reg_lambda', default_value=0.0, docstring='L
 tf.flags.DEFINE_integer(flag_name='num_classes', default_value=3510, docstring='num_classes')
 
 #   Training params
+tf.flags.DEFINE_float(flag_name='dev_sample_percentage', default_value=0.1,
+                      docstring='Percentage of the training data to use for validation')
 tf.flags.DEFINE_integer(flag_name='batch_size', default_value=64, docstring='batch size')
 tf.flags.DEFINE_float(flag_name='learning_rate', default_value=0.1, docstring='learning rate')
 tf.flags.DEFINE_boolean(flag_name='allow_soft_placement', default_value='True',
@@ -31,6 +33,9 @@ tf.flags.DEFINE_boolean(flag_name='log_device_placement', default_value='False',
                         docstring='log_device_placement ')  # 是否打印配置日志
 tf.flags.DEFINE_integer(flag_name='training_ite', default_value=900000000, docstring='training_ite')
 
+tf.flags.DEFINE_string(flag_name='model_save_path', default_value='./model/', docstring='model_save_path')
+tf.flags.DEFINE_string(flag_name='model_name', default_value='model.ckpt', docstring='model_name')
+tf.flags.DEFINE_integer(flag_name='save_freq',default_value=100000,docstring='save_freq')
 FLAGS = tf.flags.FLAGS
 # FLAGS.flag_values_dict()  # 解析参数成字典
 FLAGS._parse_flags()
