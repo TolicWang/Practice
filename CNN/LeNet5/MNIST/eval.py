e02c7607218b0fc0135939f39f4dc09252b7316b
chenng <wangchengo@126.com>
2018-08-27 19:10:44 +0800

    增加直接载入最新模型不用指定模型名称

--- CNN/LeNet5/MNIST/eval.py
+++ CNN/LeNet5/MNIST/eval.py
@@ -3,13 +3,11 @@ from MNIST.config import FLAGS
 from MNIST.cnn import CNN
 from MNIST.data_helper import gen_test_data
 cnn = CNN()
-
 with tf.Session() as sess:
     x,y_test = gen_test_data(FLAGS.test_batch_size)
     x_test = sess.run(x)
     saver = tf.train.Saver()
-    path = FLAGS.model_save_path + FLAGS.restore_model_name
-    saver.restore(sess, save_path=path)
+    saver.restore(sess, tf.train.latest_checkpoint(FLAGS.model_save_path))
     feed_dic = {cnn.input_x: x_test, cnn.input_y: y_test, cnn.dropout_keep_pro: 1.0}
     acc = sess.run(cnn.accuracy, feed_dict=feed_dic)
     print('----acc:{}---'.format(acc))
